30cdd8621dcc70fa27539b5399158e3ece595d73
vitor mouzinho <vitormouzinho12@gmail.com>
2019-12-12 20:34:56 -0500

    Add files via upload

---
+++ FacebookPosts.py
@@ -0,0 +1,19 @@
+from newsapi import NewsApiClient
+
+
+def create_news():
+    run = True;
+    while run:
+        topic = input("type a an article topic")
+        newsapi = NewsApiClient(api_key='e43b9508c4954478a8e5a1ea0f6f000c')
+        top_headlines = newsapi.get_top_headlines(q=topic, language='en')
+        content_and_url =[]
+        for m in top_headlines.get("articles"):
+            if len(top_headlines.get("articles")) > 0:
+                content_and_url.append(m.get("content"))
+                content_and_url.append(m.get("url"))
+                return content_and_url
+        print("choose a different topic, topic in top headlines is not available. ")
+
+# https://github.com/fernandoaestrella/nlp-final-project/
+content_and_url = create_news()
